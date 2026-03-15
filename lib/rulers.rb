120bbb8cf449d613a5b43e1f0a9ea4396ecd0ff1
natarajan1993 <natraj_93@yahoo.com>
2026-03-15 13:47:40 -0500

    completed chapter 2

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,30 @@
 # frozen_string_literal: true
 
 require_relative "rulers/version"
+require_relative "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        [404,
+        {'content-type' => 'text/html'}, []]
+      end
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       [200, {'content-type' => 'text/html'},
-        ["Hello from Ruby on Rulers!"]]
+        [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
